13e8dda5fb02b53ef3c36567687f6f2627373eb0
rokib <rokib@emailku.com>
2019-10-12 19:05:38 +0700

    publish file index

---
+++ application/libraries/Nusoap_lib.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Nusoap_lib{
+   
+    function Nusoap_lib(){
+        require_once('lib/nusoap.php');
+    }   
+   
+}
+
+/* 
+ * Created by cacan
+ * Email :rony.chandra.kudus@gmail.com
+ */
+
